d6fe90cb605c05aad44a09b48bb7294885538278
Leonardo Luna <93144721+leolunax@users.noreply.github.com>
2024-05-01 09:01:25 -0700

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Leonardo Luna on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
